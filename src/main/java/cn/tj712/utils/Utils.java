efa8cd52ad5d96b2da90936952c21183f45557d4
yimisiyang <15230034878@163.com>
2021-08-29 17:04:16 +0800

    del:删除target

--- src/main/java/cn/tj712/utils/Utils.java
+++ src/main/java/cn/tj712/utils/Utils.java
@@ -1,7 +1,24 @@
-package cn.tj712.utils;/**
+package cn.tj712.utils;
+
+/**
  * Created with IntelliJ IDEA.
+ *
  * @Date: 2021/8/29
- * @Time: 16:34 
+ * @Time: 16:34
  * @author: ThinkPad
- */public class Utils {
+ */
+public class Utils {
+    /**
+     * 将一个字符串型IP地址转化为long型数据
+     * @param ipStr
+     * @return
+     */
+    public long ip2Long(String ipStr){
+        String[] ips = ipStr.split("\\.");
+        long ipNum = 0;
+        for (int i = 0; i < ips.length; i++) {
+            ipNum += (Long.valueOf(ips[i]) << 8*(3 - i));
+        }
+        return ipNum;
+    }
 }
